584a7c2115d97a008a7defe6969fa3940f601f85
Albina <158292951+Olbench@users.noreply.github.com>
2025-12-14 23:55:36 +0400

    HW4 done (#7)

--- react_hw/HW4/src/App.jsx
+++ react_hw/HW4/src/App.jsx
@@ -1,15 +1,15 @@
-// import './App.css'
+import './App.css'
 import { useState } from "react";
 import CitySelector from "./components/CitySelector/CitySelector.jsx";
 import CityCard from "./components/CityCard/CityCard.jsx";
 
 function App() {
-  const [city, setCity] = useState("");
+  const [city, setCity] = useState(null);
   return (
-    <>
+    <div className="app_container">
       <CitySelector setCity={setCity} />
       <CityCard city={city} />
-    </>
+    </div>
   );
 }
 
